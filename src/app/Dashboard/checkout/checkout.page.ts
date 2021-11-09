e89f12c72ce25c4b35ae4a900174f26fe73924cd
GIGL DEV <gigl@GIGLs-MacBook-Air.local>
2021-11-09 02:12:21 +0100

    disclaimer and item list added

--- src/app/Dashboard/checkout/checkout.page.ts
+++ src/app/Dashboard/checkout/checkout.page.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { ModalController } from '@ionic/angular';
 import { TransferPaymentPage } from 'src/app/Modals/transfer-payment/transfer-payment.page';
 import { WalletPaymentPage } from 'src/app/Modals/wallet-payment/wallet-payment.page';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-checkout',
@@ -13,7 +14,9 @@ export class CheckoutPage implements OnInit {
   public qty : number = 20;
   private max_order_limit: number = 35;
   private min_order_limit: number = 2;
-  constructor(private route: Router, private modalController: ModalController) { }
+  theSate: boolean;
+_bState = false;
+  constructor(private route: Router, private modalController: ModalController, private alertController: AlertController) { }
 
   ngOnInit() {
   }
@@ -43,4 +46,28 @@ export class CheckoutPage implements OnInit {
   payWithTransfer(){
     this.route.navigate(['dashboard/pay-transfer']);
   }
+
+  async stateChange(event){
+    if(event){
+      const alert = await this.alertController.create({
+        header: 'Disclamier',
+        cssClass: 'my-custom-class',
+        message: 
+        'Ensure that the picture uploaded is the same as the item/s to be picked and it’s a shipment a bike can carry. Any fraudulent act won’t be tolerated',
+        buttons: ['Cancel', 'Proceed']
+      });
+      await alert.present();
+    }
+    else{
+        const alert = await this.alertController.create({
+          header: 'Alert',
+          cssClass: 'my-custom-class',
+          message: 'Are you sure you want go offline',
+          buttons: ['Disagree', 'Agree']
+        });
+        await alert.present();
+    console.log(event);
+  }
+}
+
 }
